6c5ce326eb408d32f16b907e935fe7c92dc091d9
Damon Kreft <damon.kreft@operationorange.uk>
2019-03-12 15:16:07 +0000

    updated tests

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -7,9 +7,12 @@ import HotelsList from '../components/HotelsList/HotelsList';
 
 export class App extends Component {
   static propTypes = {
-    data: PropTypes.arrayOf(PropTypes.shape(
-
-    )).isRequired,
+    data: PropTypes.arrayOf(PropTypes.shape({
+      id: PropTypes.number.sRequired,
+      name: PropTypes.string.sRequired,
+      rating: PropTypes.number.isRequired,
+      facilities: PropTypes.arrayOf(PropTypes.string),
+    })).isRequired,
     fetchHotelsAction: PropTypes.func.isRequired,
   };
 
--- src/containers/App.test.jsx
+++ src/containers/App.test.jsx
@@ -3,6 +3,6 @@ import React from 'react';
 import { App } from './App';
 
 it('renders without crashing', () => {
-  const wrapper = shallow(<App />);
+  const wrapper = shallow(<App data={[]} fetchHotelsAction={() => {}} />);
   expect(wrapper).toMatchSnapshot();
 });
